af19dffea7d55488adb98779dee1052377291df5
Kody Jackson <kody.s.jackson@gmail.com>
2019-05-16 13:28:58 -0500

    rolled back changes

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 
 import { StacheModule } from '@blackbaud/stache';
+// tslint:disable-next-line: max-line-length
+import { SkyListModule, SkyListToolbarModule, SkyListPagingModule, SkyListViewChecklistModule, SkyListViewGridModule, SkyListFiltersModule, SkyInlineFormModule } from '@blackbaud/skyux-builder/src/app/sky-pages.module';
 
 // Specify entry components, module-level providers, etc. here.
 @NgModule({
@@ -9,8 +11,16 @@ import { StacheModule } from '@blackbaud/stache';
   ],
   exports: [
     StacheModule
-  ],
+    ],
   providers: [],
-  entryComponents: []
+  entryComponents: [
+    SkyListModule,
+    SkyListToolbarModule,
+    SkyListPagingModule,
+    SkyListViewChecklistModule,
+    SkyListViewGridModule,
+    SkyListFiltersModule,
+    SkyInlineFormModule
+  ]
 })
 export class AppExtrasModule { }
